9394a27812ecc2a9cd9999bc9bde1ad6c13e86d5
Gero009 <GU351959@fi365.ort.edu.uy>
2026-09-08 00:35:23 -0300

    Hash recien empezado

---
+++ tads/Hash.cpp
@@ -0,0 +1,21 @@
+#ifndef HASH_IMP
+#define HASH_IMP
+
+#include "List.h"
+#include "listImp.cpp"
+
+template <class T>
+class ListImp : public List<T>
+{
+private:
+    List<T> elems;
+    int cota;
+
+public:
+
+
+};
+
+
+
+#endif;
